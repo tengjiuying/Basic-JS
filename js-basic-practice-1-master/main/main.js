3e409b333142b5f33443756f6b1d0a7c4fdd065d
TengFeng2017 <2940681539@qq.com>
2017-05-28 14:29:44 +0800

    main.js提交

---
+++ js-basic-practice-1-master/main/main.js
@@ -0,0 +1,27 @@
+// Write your cade below:
+function caclRemaindar(a,b) {return a%b};
+
+function caclSum(numArray) {
+    "use strict";
+    let sum=0;
+    numArray.forEach((item)=>{
+        sum+=item;
+    });
+    return sum; }
+
+function caclSumInConditon(numArray,num) {
+    'use strict';
+    let sum = 0 ;
+    numArray.forEach((item)=>{
+        if( item < num )
+    sum+=item;
+});
+    return sum;
+}
+
+
+module.exports = {
+    caclRemaindar,
+    caclSum,
+    caclSumInConditon
+}
